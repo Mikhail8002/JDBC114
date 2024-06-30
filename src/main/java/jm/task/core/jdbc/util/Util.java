c9e2320e52f71c306022460ea19203975c002599
Mikhail Koloskov <mkoloskov1@gmail.com>
2024-06-30 19:27:34 +0300

    задача в работе

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,39 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Util {
     // реализуйте настройку соеденения с БД
-}
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        Connection connection;
+
+        {
+            try {
+                Driver driver = new com.mysql.cj.jdbc.Driver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                if (!connection.isClosed()) {
+                    System.out.println("Соеднинение с БД установлено");
+                }
+                connection.close();
+
+                if (connection.isClosed()) {
+                    System.out.println("Соеднинение с БД закрыто");
+                }
+                } catch(SQLException e){
+                    System.err.println("Не удалось загрузить");
+
+                }
+            }
+        }
+    }
+
+
